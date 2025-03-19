221a81c9ff02248f5afa35db268222cdac3b0684
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2025-03-19 12:31:17 -0500

    Update page.tsx

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -15,7 +15,7 @@ export default function IntroPage() {
         <div className="container mx-auto px-4 pt-20">
           <div className="grid grid-cols-1 items-center gap-8 py-12 md:grid-cols-2 md:py-24">
             <div className="relative aspect-video h-full w-full overflow-hidden rounded-lg bg-black/20 backdrop-blur-sm md:aspect-square">
-              <Image src="https://cdn.vincef.org/banquet-2025/intro/banquet-picture.jpg?height=600&width=600" alt="Intro Image" fill className="object-cover" />
+              <Image src="https://cdn.vincef.org/banquet-2025/intro/banquet-picture.JPG?height=600&width=600" alt="Intro Image" fill className="object-cover" />
             </div>
             <div className="space-y-4 rounded-lg bg-black/20 p-6 backdrop-blur-sm">
               <h1 className="text-3xl font-bold tracking-tight text-white md:text-4xl">WHAT IS VINCEF BANQUET?</h1>
